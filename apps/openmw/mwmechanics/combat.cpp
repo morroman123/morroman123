bcce78ca81a749fd8b14ec955e64887ccd180921
morroman123 <anthonymorgan6674@gmail.com>
2026-07-02 08:26:19 -0700

    Update combat.cpp

--- apps/openmw/mwmechanics/combat.cpp
+++ apps/openmw/mwmechanics/combat.cpp
@@ -508,7 +508,7 @@ namespace MWMechanics
         static const float fDamageStrengthMult = MWBase::Environment::get().getWorld()->getStore().get<ESM::GameSetting>()
                 .find("fDamageStrengthMult")->mValue.getFloat();
         damage *= fDamageStrengthBase +
-                (attacker.getClass().getCreatureStats(attacker).getAttribute(ESM::Attribute::Strength).getModified() * fDamageStrengthMult * 0.1f);
+                (attacker.getClass().getCreatureStats(attacker).getAttribute(ESM::Attribute::Strength).getModified() * fDamageStrengthMult * 0.1f + skillValue);
     }
 
     void getHandToHandDamage(const MWWorld::Ptr &attacker, const MWWorld::Ptr &victim, float &damage, bool &healthdmg, float attackStrength)
