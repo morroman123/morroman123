2fb5e87c04bbf0264574c45ae7fae1c1c61d65e1
morroman123 <anthonymorgan6674@gmail.com>
2026-07-02 13:37:42 -0700

    Update actor.cpp

--- apps/openmw/mwclass/actor.cpp
+++ apps/openmw/mwclass/actor.cpp
@@ -101,7 +101,7 @@ namespace MWClass
         if (movementSettings.mIsStrafing)
             moveSpeed *= 0.75f;
         if (movementSettings.mIsBackpedal)
-            moveSpeed *= 0.15f;
+            moveSpeed *= 0.5f;
         return moveSpeed;
     }
 }
