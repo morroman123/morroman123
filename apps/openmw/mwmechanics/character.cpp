b4042d7817b07273906345db10472ad81acc4939
morroman123 <anthonymorgan6674@gmail.com>
2026-07-03 11:13:27 -0700

    Update character.cpp

--- apps/openmw/mwmechanics/character.cpp
+++ apps/openmw/mwmechanics/character.cpp
@@ -289,7 +289,7 @@ void CharacterController::refreshHitRecoilAnims(CharacterState& idle)
                     mCurrentHit = anim;
 
                     float stunChance = Misc::Rng::rollProbability();
-                    float agilityLocal = 0.2 + (   stats.getAttribute(ESM::Attribute::Agility).getModified()  );
+                    float agilityLocal = mPtr.getClass().getCreatureStats(mPtr).getAttribute(ESM::Attribute::Agility).getModified();
                     if (stunChance >= 2/3.f)
                     mAnimation->play(mCurrentHit, Priority_Hit, MWRender::Animation::BlendMask_All, true, agilityLocal, "start", "stop", 0.0f, 0);
                 }
