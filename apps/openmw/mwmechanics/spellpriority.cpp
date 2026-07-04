ab146444060340bd3712367d553b1d7248c79a8c
morroman123 <anthonymorgan6674@gmail.com>
2026-07-04 12:06:06 -0700

    Update spellpriority.cpp

--- apps/openmw/mwmechanics/spellpriority.cpp
+++ apps/openmw/mwmechanics/spellpriority.cpp
@@ -238,7 +238,7 @@ namespace MWMechanics
                 osg::Vec3f actorPos = actor.getRefData().getPosition().asVec3();
                 osg::Vec3f enemyPos = enemy.getRefData().getPosition().asVec3();
 
-                float distTest = (actorPos.z - enemyPos.z).length();
+                float distTest = (actorPos.z() - enemyPos.z());
                 
                 if (distTest <= -100)
                     return 1000.f;
