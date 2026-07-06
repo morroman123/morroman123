f400e6a08cd461a50eb8cbcaf117da7790a0a549
morroman123 <anthonymorgan6674@gmail.com>
2026-07-05 19:54:16 -0700

    Update spellpriority.cpp

--- apps/openmw/mwmechanics/spellpriority.cpp
+++ apps/openmw/mwmechanics/spellpriority.cpp
@@ -129,10 +129,9 @@ namespace MWMechanics
             const ESM::Race* race = MWBase::Environment::get().getWorld()->getStore().get<ESM::Race>().find(raceid);
             if (race->mPowers.exists(spell->mId))
                 float rand = Misc::Rng::rollProbability();
-                if (rand >= 2/3.f)
+                
                     return 0.f;
-                else
-                break
+                
 
             
         }
