b9af8a3b6d2181a1c539760bcc7edfb53af60af5
morroman123 <anthonymorgan6674@gmail.com>
2026-07-09 08:16:45 -0700

    Update spellpriority.cpp

--- apps/openmw/mwmechanics/spellpriority.cpp
+++ apps/openmw/mwmechanics/spellpriority.cpp
@@ -122,7 +122,7 @@ namespace MWMechanics
         if (spell->mData.mType != ESM::Spell::ST_Spell)
         {
             std::optional<float> rand;
-            if ( rand.has_value() )
+            if ( !rand.has_value() )
              rand = Misc::Rng::rollProbability();
 
             if (rand >= 50/100.f)
