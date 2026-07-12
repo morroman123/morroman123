7ae1c56f18000d31beeaa2f1f6b92a1149adf7a9
morroman123 <anthonymorgan6674@gmail.com>
2026-07-12 12:51:29 -0700

    Update actors.cpp

--- apps/openmw/mwmechanics/actors.cpp
+++ apps/openmw/mwmechanics/actors.cpp
@@ -877,7 +877,7 @@ namespace MWMechanics
         double fort = 0 +
             creatureStats.getMagicEffects().get (EffectKey (ESM::MagicEffect::FortifyMagicka)).getMagnitude() * 1;
 
-        float base = (intelligence * magickaFactor) + fort;
+        float base = (intelligence * (static_cast<float>(magickaFactor)) + (static_cast<float>(fort)));
 
         //float fort = 0 + creatureStats.getMagicEffects().get (EffectKey (ESM::MagicEffect::FortifyMagicka)).getMagnitude() * 0.1;//edit
         //DynamicStat<float> magicka = (static_cast<float>(fort)) + creatureStats.getMagicka();
