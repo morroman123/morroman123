1696842b9bfb38cb4e271ef5346fd1c04e2e1487
morroman123 <anthonymorgan6674@gmail.com>
2026-07-14 16:58:28 -0700

    Update actors.cpp

--- apps/openmw/mwmechanics/actors.cpp
+++ apps/openmw/mwmechanics/actors.cpp
@@ -466,6 +466,15 @@ namespace MWMechanics
     {
         //
 
+        if (ptr == getPlayer())//edit block
+        {
+            DynamicStat<float> health = ptr.getClass().getCreatureStats(ptr).getHealth();//edit
+                if (health.GetCurrent() > health.GetBase())
+                {
+                    health.SetBase(health.GetCurrent());
+                    ptr.getClass().getCreatureStats(ptr).setHealth(health);
+                }
+        }
         
         
         // magic effects
@@ -896,10 +905,10 @@ namespace MWMechanics
         //magicka.setCurrent(1, false, true);//edit
         //magicka.setModified((magicka.getBase() + fort), 0);//recent1setsmagickato100works
         //magicka.setBase(magicka.getModified());//recent1setsmagickato100works
-        health.setBase(magicka.getModified());//edit
+        //health.setBase(magicka.getModified());//edit
         //fatigue.setModified(magicka.getModified(), 0);//edit
         creatureStats.setMagicka(magicka);
-        creatureStats.setHealth(health);//edit
+        //creatureStats.setHealth(health);//edit
         //creatureStats.setFatigue(fatigue);//edit
     }
 
