234057b727e8bd10c40e7a4678f5d06b7015da71
morroman123 <anthonymorgan6674@gmail.com>
2026-07-15 17:45:22 -0700

    Update actors.cpp

--- apps/openmw/mwmechanics/actors.cpp
+++ apps/openmw/mwmechanics/actors.cpp
@@ -499,7 +499,7 @@ namespace MWMechanics
                 float fatigueSet = (strength + endurance + agility + willpower + fortFatigue);
                 float strengthMod = (strength - strengthBase);
                 float enduranceMod = (endurance - enduranceBase); 
-                float healthSet = ((strengthBase + strengthMod) * 0.2 ) + ((enduranceBase + enduranceMod) * 1.5) + fortHealth) + (level * 1.5);
+                float healthSet = ((strengthBase + strengthMod) * 0.2 ) + (((enduranceBase + enduranceMod) * 1.5) + fortHealth) + (level * 1.5);
 
                 if( health.getBase() != healthSet )
                 {
