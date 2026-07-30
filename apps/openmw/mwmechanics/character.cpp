456de9a915d3836f90708ddbe09f3f3706f06eb3
morroman123 <anthonymorgan6674@gmail.com>
2026-07-29 19:26:36 -0700

    Update character.cpp

--- apps/openmw/mwmechanics/character.cpp
+++ apps/openmw/mwmechanics/character.cpp
@@ -2971,12 +2971,16 @@ void CharacterController::setVisibility(float visibility)
     if (mPtr.getClass().isActor())
     {
         //edit
-        float alpha = 1f;
+        float alpha = 1.f;
         //bool sneakTest = mPtr.getClass().getCreatureStats(mPtr).getStance(MWMechanics::CreatureStats::Stance_Sneak);
-        if mPtr.getClass().getCreatureStats(mPtr).getStance(MWMechanics::CreatureStats::Stance_Sneak) == true
+        if (mPtr.getClass().getCreatureStats(mPtr).getStance(MWMechanics::CreatureStats::Stance_Sneak)) == true
+        {
          alpha = 0.1f;
-        if mPtr.getClass().getCreatureStats(mPtr).getStance(MWMechanics::CreatureStats::Stance_Sneak) == false
+        }
+        if (mPtr.getClass().getCreatureStats(mPtr).getStance(MWMechanics::CreatureStats::Stance_Sneak)) == false
+        {
          alpha = 1.f;
+        }
         if (mPtr.getClass().getCreatureStats(mPtr).getMagicEffects().get(ESM::MagicEffect::Invisibility).getModifier()) // Ignore base magnitude (see bug #3555).
         {
             if (mPtr == getPlayer())
